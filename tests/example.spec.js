93b8cee87040e5cf6a33f9751aa15f6502090e11
cir94 <claire.isabella.ramos@gmail.com>
2024-06-07 14:15:22 -0400

    feat: changed code in example.spec.js to have simple test to open UI Testing Playground site for testing purposes

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,19 +1,25 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
+import { test, expect } from '@playwright/test';
+
+test.use({
+  ignoreHTTPSErrors: true,
+});
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://uitestingplayground.com/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Playground/);
 });
 
 test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://uitestingplayground.com/');
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByRole('link', { name: 'Click' }).click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(
+    page.getByRole('button', { name: 'Button That Ignores DOM Click Event' })
+  ).toBeVisible();
 });
