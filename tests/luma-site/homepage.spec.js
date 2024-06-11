e9f852592c17b98689b79894ef2f5cf4ac92498c
cir94 <claire.isabella.ramos@gmail.com>
2024-06-11 19:45:59 -0400

    feat: added prelim tests to homepage.spec.js for luma-site

--- tests/luma-site/homepage.spec.js
+++ tests/luma-site/homepage.spec.js
@@ -4,3 +4,12 @@ import { test, expect } from '@playwright/test';
 test.beforeEach(async ({ page }) => {
   await page.goto('https://magento.softwaretestingboard.com/');
 });
+
+test('at homepage', async ({ page }) => {
+  await expect(page).toHaveTitle(/Home Page/);
+});
+
+test('has navbar', async ({ page }) => {
+  const navbar = page.getByRole('menu');
+  await expect(navbar).toHaveCount(6);
+});
